eadd6499b9f80108dad5df5c7244a56a52dcdb16
HarllonCS <harllon.santana@gmail.com>
2023-07-22 21:18:37 -0300

    new_layout
    Notebook clone

--- js/script.js
+++ js/script.js
@@ -1,15 +1,1 @@
-var bd = document.body
-var divLogin = document.querySelector('#cont-login')
-var divSignUp = document.querySelector('#cont-signup')
-
-bd.removeChild(divSignUp)
-
-document.querySelector('#switch-login').addEventListener('click', () => {
-    bd.removeChild(divLogin)
-    bd.appendChild(divSignUp)
-
-    document.querySelector('#switch-signup').addEventListener('click', () => {
-        bd.removeChild(divSignUp)
-        bd.appendChild(divLogin)
-    })
-})
+function changeLang(value, file) {location.assign(`../${value}/${file}.html`)}
