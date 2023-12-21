6daf520dc19949c7de519ce205d606581e5d5fa5
HarllonCS <harllon.santana@gmail.com>
2023-12-21 00:38:00 -0300

    Switch screen between Login and Register screen

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
+let register = document.querySelector('#create_acc')
+let login =  document.querySelector('#login')
+let reg_p = document.querySelector('#reg_p')
+let log_p = document.querySelector('#login_p')
+
+reg_p.addEventListener('click', () => {
+    register.style.display = 'none'
+    login.style.display = 'block'
+})
+
+log_p.addEventListener('click', () => {
+    register.style.display = 'block'
+    login.style.display = 'none'
+})
+
 function changeLang(value, file) {location.assign(`../${value}/${file}.html`)}
